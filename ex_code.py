aa5a5b31681dd33fd3d8807fb9d396c58e0120fa
kanderson666 <anderson.kj66@gmail.com>
2026-05-04 16:45:17 +0000

    py139 study

--- ex_code.py
+++ ex_code.py
@@ -1,47 +1,53 @@
 class CustomSet:
-    def __init__(self, elements=None):
-        if elements is None:
-            elements = []
-        self._elements = []
-        for el in elements:
-            if el not in self._elements:
-                self._elements.append(el)
-
+    def __init__(self, lst=[]):
+        self.items = []
+        self.add_items(lst)
+    
     def is_empty(self):
-        return len(self._elements) == 0
+        return len(self.items) == 0
 
-    def intersection(self, other_set):
-        same_elements = [el for el in self._elements if other_set.contains(el)]
-        return CustomSet(same_elements)
+    def contains(self, num):
+        return num in self.items
 
-    def union(self, other_set):
-        union_set = CustomSet(self._elements)
-        for el in other_set._elements:
-            union_set.add(el)
-        return union_set
+    def is_subset(self, other):
+        for item in self.items:
+            if item not in other.items:
+                return False
+        return True
 
-    def difference(self, other_set):
-        different_elements = [el for el in self._elements if not other_set.contains(el)]
-        return CustomSet(different_elements)
+    def is_disjoint(self, other):
+        for item in self.items:
+            if item in other.items:
+                return False
+        return True
 
-    def is_disjoint(self, other_set):
-        return all(not other_set.contains(el) for el in self._elements)
-
-    def is_same(self, other_set):
-        if len(self._elements) != len(other_set._elements):
+    def is_same(self, other):
+        if len(self) != len(other):
             return False
-        return all(other_set.contains(el) for el in self._elements)
+        return self.is_subset(other)
 
-    def is_subset(self, other_set):
-        return all(other_set.contains(el) for el in self._elements)
+    def add(self, num):
+        if num not in self.items:
+            self.items.append(num)
+    
+    def add_items(self, lst):
+        for item in lst:
+            self.add(item)
 
-    def add(self, element):
-        if element not in self._elements:
-            self._elements.append(element)
-        return self
+    def intersection(self, other):
+        return CustomSet([item for item in self.items if item in other.items])
+    
+    def difference(self, other):
+        return CustomSet([item for item in self.items if item not in other.items])
+    
+    def union(self, other):
+        result = CustomSet(self.items)
+        result.add_items(other.items)
 
-    def contains(self, element):
-        return element in self._elements
+        return result
 
-    def __eq__(self, other_set):
-        return self.is_same(other_set)
+    def __eq__(self, other):
+        return self.is_same(other)
+    
+    def __len__(self):
+        return len(self.items)
